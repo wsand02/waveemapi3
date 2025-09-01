b6ae4c9a6e3b485fbc60e1add8a777e274da0218
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2025-09-01 19:34:31 +0200

    add invalid auth tests

--- src/api/status.rs
+++ src/api/status.rs
@@ -12,31 +12,6 @@ fn status() -> Json<StatusResp> {
 }
 
 #[derive(Serialize)]
-struct StatusResp {
-    status: String,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::rocket;
-    use rocket::local::blocking::Client;
-    use rocket::serde::json;
-    #[test]
-    fn test_status_json_structure() {
-        let client = Client::tracked(rocket()).expect("valid rocket instance");
-        let response = client.get("/api/status").dispatch();
-        let body = response.into_string().unwrap();
-        let parsed: json::Value = json::from_str(&body).unwrap();
-        assert_eq!(parsed["status"], "Online");
-    }
-
-    #[test]
-    fn test_status_resp_serialization() {
-        let resp = StatusResp {
-            status: "Online".to_string(),
-        };
-        let jsonser = json::to_string(&resp).unwrap();
-        assert_eq!(jsonser, r#"{"status":"Online"}"#);
-    }
+pub(crate) struct StatusResp {
+    pub status: String,
 }
